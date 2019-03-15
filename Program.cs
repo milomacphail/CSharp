1c1d67333813407b0044fd52ad9c705204b06585
Milo MacPhail <40954168+milomacphail@users.noreply.github.com>
2019-03-15 17:03:25 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Definition for singly-linked list:
+// class ListNode<T> {
+//   public T value { get; set; }
+//   public ListNode<T> next { get; set; }
+// }
+//
+ListNode<int> removeKFromList(ListNode<int> l, int k)
+{
+    if (l == null)
+    {
+        return l;
+    }
+    l.next = removeKFromList(l.next, k);
+    if (l.value == k)
+    {
+        return l.next;
+    }
+    return l;
+
+}
+
