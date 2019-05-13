78493996aa17b37cdde82aa875d9508e9b6e4b23
milomacphail <milomacphail@gmail.com>
2019-05-12 20:40:47 -0600

    FileInfoDemo methods

---
+++ FileInfoDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace CSharpFundamentals
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+
+            if (fileInfo.Exists)
+            {
+                //
+            }
+            fileInfo.OpenRead;
+        }
+    }
+}
